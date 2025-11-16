ff71d880171243d64882b8294d58f7c819ae178e
Pratik07007 <s.dhimal006@gmail.com>
2025-11-16 07:04:26 +0545

    bug:build issue in main

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
         <Toaster position="top-center" />
-        {children}
+        <Suspense fallback={<>Loading...</>}>
+          {children}
+        </Suspense>
       </body>
     </html >
   );
--- server/src/server.ts
+++ server/src/server.ts
@@ -15,6 +15,7 @@ app.use(
 );
 
 app.use(express.json());
+
 app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
